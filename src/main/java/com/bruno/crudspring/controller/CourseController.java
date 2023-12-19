da903abb5c00e8b110c6910480cb7d98d3dc1533
bruno-pequeno <bru.pequeno@gmail.com>
2023-12-18 21:44:58 -0300

    feat: Creating mapping to add courses

--- src/main/java/com/bruno/crudspring/controller/CourseController.java
+++ src/main/java/com/bruno/crudspring/controller/CourseController.java
@@ -4,9 +4,9 @@ import com.bruno.crudspring.model.Course;
 import com.bruno.crudspring.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +21,15 @@ public class CourseController {
     @GetMapping
     public List<Course> findAll() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    public Course save(@RequestBody Course courseData){
+        return courseRepository.save(courseData);
+    }
+
+    @DeleteMapping
+    public void delete(@RequestBody Long idCourse) {
+        courseRepository.deleteById(idCourse);
     }
 }
